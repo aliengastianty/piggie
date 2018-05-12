868f95be619ae23bbf20a000c834811cc81391a8
yantisa <38687597+yantisa@users.noreply.github.com>
2018-05-12 22:24:22 +0800

    Add files via upload
    menambah kodingan di rakun pada saat game over  setLocation(50,210); 
                   {
                       if (isTouching(Pohon1.class)){
                           Greenfoot.stop();
                           getWorld().addObject(new Kalah() , 300, 210);

---
+++ piggie/piggie/Khun.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Khun here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Khun extends Actor
+{
+    int a=2;//percepatan
+    int v=1;//kecepatan
+    /**
+     * Act - do whatever the Khun wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+     // Add your action code here.
+        setLocation(getX(),getY()+1+v);
+        v+=a;//v=v+a
+        
+        if(Greenfoot.isKeyDown("up")){
+            v=-20;
+            }       
+            else
+               setLocation(50,210); 
+               {
+                   if (isTouching(Pohon1.class)){
+                       Greenfoot.stop();
+                       getWorld().addObject(new Kalah() , 300, 210);
+                    }
+        }
+
+    }
+    }
+
+
+
+     

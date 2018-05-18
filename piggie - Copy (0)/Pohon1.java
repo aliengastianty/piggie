8e34681e31ac1a2620f32bb5fa843840f350d8dc
yantisa <38687597+yantisa@users.noreply.github.com>
2018-05-18 11:32:41 +0800

    Add files via upload
    menambahkan kodingan lopping pada pohon

---
+++ piggie - Copy (0)/Pohon1.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Pohon1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pohon1 extends Actor
+{
+    /**
+     * Act - do whatever the Pohon1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(getX()-1,getY());
+        World w=getWorld();
+        if(isAtEdge())
+        {
+            w.removeObject(this);
+       
+        
+         }
+   }
+}
+
+
